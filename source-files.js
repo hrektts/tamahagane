e6728eea3283c050333c4d6ee1a89f99988fcb21
ci <ci>
2021-11-03 01:50:36 +0000

    init

---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["tamahagane"] = {"name":"","dirs":[{"name":"array","files":["fmt.rs","iter.rs","mod.rs","ops.rs"]},{"name":"dimension","files":["mod.rs","ops.rs"]},{"name":"shape","files":["mod.rs","new_shape.rs"]},{"name":"storage","files":["cow.rs","mod.rs","owned.rs","routine.rs","shared.rs","view.rs"]}],"files":["array_index.rs","error.rs","lib.rs","order.rs","routine.rs","slice.rs","slice_info.rs","util.rs"]};
+createSourceSidebar();
---
+++ tamahagane/storage/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["StorageImpl",""]],"trait":[["Storage",""],["StorageMut",""],["StorageOwned",""]]});
